9bf938aa0eb800e0fdfd1c893b0e3efd32cc01ff
shoevakramchik@gmail.com <shoevakramchik@gmail.com>
2024-06-10 00:42:01 +0300

    Добавьте файлы проекта.

---
+++ Dorabotka(4).ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorabotka_4_.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите количество строк для матрицы: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Напишите количество столбцов для матрицы: ");
+            int col = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[row, col];
+            Random r = new Random();
+            int sum = 0;
+            for (int i = 0; i < row; i++)
+            {               
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = r.Next(10);
+                    sum += matrix[i, j];
+                    Console.Write($" {matrix[i, j]}");                                      
+                }   
+                Console.WriteLine("\n");
+                
+            }
+            Console.WriteLine($"Сумма всех элементов: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dorabotka(4.1).ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorabotka_4._1_.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите количество строк для матриц: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Напишите количество столбцов для матриц: ");
+            int col = int.Parse(Console.ReadLine());
+            int[,] matrixA = new int[row, col];
+            int[,] matrixB = new int[row, col];
+            int[,] matrixC = new int[row, col];
+            Random r = new Random();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrixA[i, j] = r.Next(10);
+                    Console.Write($"{matrixA[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrixB[i, j] = r.Next(10);
+                    Console.Write($"{matrixB[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrixC[i, j] = matrixA[i, j] + matrixB[i, j];
+                    Console.Write($"{matrixC[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
